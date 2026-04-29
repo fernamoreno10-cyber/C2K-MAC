d545903b2fa3f3d9385fb1590f7dd034f742a07f
fernamoreno10-cyber <ferna.moreno10@gmail.com>
2026-04-29 12:18:48 -0500

    feat: add OverlayWindowController at .screenSaver window level

---
+++ Sources/KeyboardCleanerLib/OverlayWindowController.swift
@@ -0,0 +1,31 @@
+import Cocoa
+import SwiftUI
+
+class OverlayWindowController {
+    private var window: NSWindow?
+
+    func show(state: AppState, onDone: @escaping () -> Void) {
+        guard let screen = NSScreen.main else { return }
+
+        let hosting = NSHostingController(rootView: OverlayView(state: state, onDone: onDone))
+
+        let win = NSWindow(
+            contentRect: screen.frame,
+            styleMask: [.borderless],
+            backing: .buffered,
+            defer: false
+        )
+        win.level = .screenSaver
+        win.collectionBehavior = [.canJoinAllSpaces, .fullScreenAuxiliary]
+        win.isOpaque = true
+        win.backgroundColor = NSColor(red: 0.961, green: 0.961, blue: 0.969, alpha: 1)
+        win.contentViewController = hosting
+        win.makeKeyAndOrderFront(nil)
+        self.window = win
+    }
+
+    func hide() {
+        window?.close()
+        window = nil
+    }
+}
